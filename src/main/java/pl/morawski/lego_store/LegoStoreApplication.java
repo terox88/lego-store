fabcfe538b5b2dc444952d45fe2e149c3e6fd96f
Michal <terox@wp.pl>
2026-02-24 20:16:23 +0100

    chore: configure docker-compose

---
+++ src/main/java/pl/morawski/lego_store/LegoStoreApplication.java
@@ -0,0 +1,13 @@
+package pl.morawski.lego_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoStoreApplication.class, args);
+	}
+
+}
