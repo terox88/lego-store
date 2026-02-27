95243a16c8f2f1be75d7bfdd12425ee9bd09238b
Michal <terox@wp.pl>
2026-02-27 19:03:30 +0100

    chore: add authorization to OpenApi

--- src/main/java/pl/morawski/lego_store/configuration/OpenApiConfig.java
+++ src/main/java/pl/morawski/lego_store/configuration/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package pl.morawski.lego_store.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,5 +18,18 @@ public class OpenApiConfig {
                         .title("Lego Store API")
                         .description("REST API for managing Lego sets")
                         .version("1.0.0"));
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
